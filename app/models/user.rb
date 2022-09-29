21523c3022b58d711d557a00fa27184a0678f240
Siva.R <venthansiva111@gmail.com>
2022-09-29 15:56:52 +0530

    1. Included spec for  Model & controller

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,11 @@
 class User < ApplicationRecord
+  validates_presence_of :email_id
   validates :email_id, uniqueness: true
-
+  validates_presence_of :firstname
+  validates_presence_of :address
+  validates_presence_of :contact_no
+  validates_presence_of :dob
+  
   has_many :user_educations, dependent: :destroy
   #to upload image
   has_one_attached :image
